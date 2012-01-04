551c99dbc7ff24b3a66fa926d086ac181599bff5
Bernard Potocki <bernard.potocki@imanel.org>
2012-01-05 00:43:55 +0100

    Remove benchmark

--- web.js
+++ web.js
@@ -15,13 +15,10 @@ function do_query(query, callback) {
 do_query('apple inc', function(error, response, body) {
   if(!error && response.statusCode == 200) {
     var $ = cheerio.load(body);
-    var start = new Date();
     var list = ($('#disambigbox').size() == 0);
     var content_html = $('#bodyContent .mw-content-ltr p').html();
     var content = cheerio.load(content_html);
     content('sup').remove();
     console.log( content('p').text() );
-    var end = new Date();
-    console.log("ops took: " + (end.getTime() - start.getTime()) + " ms");
   }
 });
