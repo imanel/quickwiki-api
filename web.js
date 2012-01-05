cb85e79521194e17403c6495a5f472e9645a91d2
Bernard Potocki <bernard.potocki@imanel.org>
2012-01-05 01:38:08 +0100

    Split to functions

--- web.js
+++ web.js
@@ -12,16 +12,39 @@ function do_query(query, callback) {
   }, callback)
 }
 
+function parseData(content) {
+  var documentBody, bodyContent, list, result;
+
+  documentBody = cheerio.load(content),
+  bodyContent = documentBody('#bodyContent');
+
+  list = ( bodyContent.find('#disambigbox').size() > 0);
+
+  if(list) {
+    result = parseList(bodyContent);
+  } else {
+    result = parseText(bodyContent);
+  }
+  return result
+}
+
+function parseText(content) {
+  var contentWrapper,contentHTML,paragraph;
+  contentWrapper = content.find('.mw-content-ltr');
+  contentWrapper.remove('table');
+  contentHTML = contentWrapper.find('p').html();
+  paragraph = cheerio.load(contentHTML);
+  paragraph('sup').remove();
+  return { type: 'text', data: paragraph('p').text() }
+}
+
+function parseList(content) {
+  return { type: 'list', data: [] }
+}
+
 do_query('apple inc', function(error, response, body) {
   if(!error && response.statusCode == 200) {
-    var $ = cheerio.load(body);
-    var bodyContent = $('#bodyContent');
-    var list = ( bodyContent.find('#disambigbox').size() == 0);
-    var content_wrapper = bodyContent.find('.mw-content-ltr');
-    content_wrapper.remove('table');
-    var content_html = content_wrapper.find('p').html();
-    var content = cheerio.load(content_html);
-    content('sup').remove();
-    console.log( content('p').text() );
+    var result = parseData(body);
+    console.log( { result: result.type, data: result.data } );
   }
 });
